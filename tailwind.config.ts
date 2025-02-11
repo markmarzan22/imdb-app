194fd4e8e9beaab1bd5bbf2ec2c3d6ca83147f81
Mark Marzan <markmarzan@guroolearning.com>
2025-02-11 18:28:31 +0800

    Integrate material tailwind UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,18 @@
 import type { Config } from 'tailwindcss';
+import withMT from '@material-tailwind/react/utils/withMT';
 
-export default {
+const config = withMT({
     content: [
         './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+        './node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
+        './node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}'
     ],
     theme: {
-        extend: {
-            colors: {
-                background: 'var(--background)',
-                foreground: 'var(--foreground)'
-            }
-        }
+        extend: {}
     },
     plugins: []
-} satisfies Config;
+}) as Config;
+
+export default config;
