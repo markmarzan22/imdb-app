4f8c1d555709af16689d32fb42a5ba3bd714e70e
Mark Marzan <markmarzan@guroolearning.com>
2025-02-11 20:34:07 +0800

    feat: mobile view

--- src/components/search/SearchMovieResult.tsx
+++ src/components/search/SearchMovieResult.tsx
@@ -4,7 +4,7 @@ import SearchMovieCard from './SearchMovieCard';
 const SearchMovieResult = () => {
     const { movies } = useAppContext();
     return (
-        <div className="max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
+        <div className="max-w-screen-xl mx-auto grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 my-6 px-6">
             {movies.map((movie) => (
                 <SearchMovieCard key={movie.imdbID} movie={movie} />
             ))}
