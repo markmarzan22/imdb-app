6feb4eb073c2b4d201270c814a66d4c555f02b86
Mark Marzan <markmarzan@guroolearning.com>
2025-02-12 10:27:32 +0800

    Create useSearchMovies custom hook

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -6,14 +6,43 @@ import React, { createContext, useContext, useState, ReactNode } from 'react';
 interface AppState {
     movies: MovieOverview[];
     setMovies: React.Dispatch<React.SetStateAction<MovieOverview[]>>;
+    query: string;
+    setQuery: React.Dispatch<React.SetStateAction<string>>;
+    currentPage: number;
+    setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
+    isLoading: boolean;
+    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+    hasMore: boolean;
+    setHasMore: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const AppContext = createContext<AppState | undefined>(undefined);
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
     const [movies, setMovies] = useState<MovieOverview[]>([]);
+    const [query, setQuery] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [isLoading, setIsLoading] = useState(false);
+    const [hasMore, setHasMore] = useState(true);
 
-    return <AppContext.Provider value={{ movies, setMovies }}>{children}</AppContext.Provider>;
+    return (
+        <AppContext.Provider
+            value={{
+                movies,
+                setMovies,
+                query,
+                setQuery,
+                currentPage,
+                setCurrentPage,
+                isLoading,
+                setIsLoading,
+                hasMore,
+                setHasMore
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    );
 };
 
 export const useAppContext = () => {
