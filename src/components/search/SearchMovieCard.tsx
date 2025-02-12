32f4c6a81c2a43610ac947fddc956e7fd03515d9
Mark Marzan <markmarzan@guroolearning.com>
2025-02-12 10:30:20 +0800

    Implement paginatin on page scroll

--- src/components/search/SearchMovieCard.tsx
+++ src/components/search/SearchMovieCard.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { MovieOverview } from '@/app/models/movieOverview.model';
 import { Card, CardHeader, CardBody, Typography, Chip } from '@material-tailwind/react';
 
@@ -10,7 +12,7 @@ const SearchMovieCard: React.FC<SearchMovieCardProps> = ({ movie }) => {
     return (
         <Card className="max-w-[24rem] overflow-hidden">
             <CardHeader floated={false} shadow={false} color="transparent" className="m-0 rounded-none">
-                <img src={Poster} alt="ui/ux review check" className="w-full h-[200px] object-cover" />
+                <img src={Poster} alt={Title} className="w-full h-[200px] object-cover" />
             </CardHeader>
             <CardBody>
                 <div className="flex mb-2">
