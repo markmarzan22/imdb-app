fc377f6d6c4ff761436621f99ed352cf4e6618d5
Mark Marzan <markmarzan@guroolearning.com>
2025-02-12 11:58:20 +0800

    Remove unused theme provider

--- src/app/ThemeProviderWrapper.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { ThemeProvider } from '@material-tailwind/react';
-
-export function ThemeProviderWrapper({ children }: { children: React.ReactNode }) {
-    return <ThemeProvider>{children}</ThemeProvider>;
-}
